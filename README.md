9cee959e6b7fef4653590ae53518b65349a1e06a
Dan Parkhomenko <parkh.dan@gmail.com>
2022-08-16 23:52:40 +0300

    Init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Dev Env
+
+Basic Vim, Tmux, Zsh dev env for the Deep Learning AMI Amazon Linux 2.
+
+## Getting started
+
+### Commandline Deployment
+> Need modify config/deploy.ini before running your commands
+
+    git clone https://github.com/parkh/dev-env.git
+    cd dev-env
+    ./deploy.sh <STACK_NAME> <REGION> <PROFILE>
---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -ex
+
+STACK_NAME=${1:-dev-env}
+REGION=${2:-eu-central-1}
+PROFILE=${3:-default}
+
+deploy () {
+
+  local CMD="aws cloudformation --region=${REGION} --profile=${PROFILE}"
+
+  ${CMD} deploy \
+  --stack-name ${STACK_NAME} \
+  --template-file ./templates/ec2.yml \
+  --parameter-overrides $(cat config/deploy.ini) \
+  --capabilities CAPABILITY_NAMED_IAM CAPABILITY_IAM CAPABILITY_AUTO_EXPAND
+}
+
+deploy
